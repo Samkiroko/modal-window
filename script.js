ca0447e314dfd536c2a80d09a16b90e7994eee03
Samkiroko <kirokodev@gmail.com>
2021-02-19 15:53:36 +0300

    Working with classes

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 'use strict';
 
-const model = document.querySelector('.modal');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 }
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
